9eaa7dfc12791c6bee4c66c68ed97d73d19ea348
CheBeomHee <tigerfrom2@naver.com>
2024-12-26 20:47:53 +0900

    fix: url String from .yml file

--- src/main/java/resume/backend/user/controller/PageController.java
+++ src/main/java/resume/backend/user/controller/PageController.java
@@ -1,12 +1,19 @@
 package resume.backend.user.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
+    @Value("${custom.prefix}")
+    private String prefix;
+
     @GetMapping("/home")
-    public String homePage(){
+    public String homePage(Model model){
+        model.addAttribute("prefix", prefix);
+
         return "home";
     }
 }
